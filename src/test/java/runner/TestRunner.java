ddf641fe4813809065f4e5c58911862eaaaf71f8
sakshijamadar <sakshijamadar2608@gmail.com>
2024-04-04 12:46:28 +0530

    first commit

---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,18 @@
+package runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+			features = {"src/test/resources/Features"},
+			glue = {"steps"},
+			plugin = {"pretty",
+	                "html:target/cucumber-reports/cucumber.html",
+	                "json:target/cucumber-reports/cucumber.json"},
+			tags = "@MenuCheck",
+			monochrome = true
+		)
+public class TestRunner extends AbstractTestNGCucumberTests{
+
+	
+}
